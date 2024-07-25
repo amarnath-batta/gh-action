e17dc34755b19dfb370b55aa1ca5e68e3ad8179c
amarnath-batta <67098022+amarnath-batta@users.noreply.github.com>
2024-07-25 13:50:51 -0500

    Create Sample.java

---
+++ src/main/java/Sample.java
@@ -0,0 +1,21 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+	
+	public static void main(String[] args) throws IOException, InterruptedException {
+		ProcessBuilder pbGrep = new ProcessBuilder();
+		List<String> pbArguments = new ArrayList<>();
+
+        pbArguments.add("grep");
+        pbArguments.add(" -Pnr ");
+        pbArguments.add(" --include=*.properties ");
+        pbArguments.add(" (?i)^.*(password|pwd|passwd)\\\\s*[=:]\\\\s*(?!cyberark|false)\\\\S+\\r?$");
+        pbGrep.command(pbArguments);
+        Process pgrep = pbGrep.start();
+        pgrep.waitFor();
+        System.out.println("Exit value is : " + pgrep.exitValue());
+	}
+
+}
