07d2647b1a05cc9409e5789eee8cb383f8d7fc9e
amarnath-batta <67098022+amarnath-batta@users.noreply.github.com>
2024-07-30 15:38:59 -0500

    Update SampleClassTest.java

--- src/test/java/com/example/demo/SampleClassTest.java
+++ src/test/java/com/example/demo/SampleClassTest.java
@@ -3,14 +3,20 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SampleClassTest {
 	
 	@Test
-	public void testAdd() {
-		SampleClass obj = new SampleClass();
-		int sum = obj.add(1, 2);
-		assertEquals(3, sum);
+	public void testAdd()  throws IOException, InterruptedException  {
+		ProcessBuilder pbGrep = new ProcessBuilder();
+		List<String> pbArguments = new ArrayList<>();
+        pbGrep.command("grep -Pnr --include=*.properties demo");
+        Process pgrep = pbGrep.start();
+        pgrep.waitFor();
+        System.out.println("Exit value is : " + pgrep.exitValue());
 	}
 
 }
