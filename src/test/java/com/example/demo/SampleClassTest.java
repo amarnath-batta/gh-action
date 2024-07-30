38809f0518179c555f1732f9e8248561204416a5
amarnath-batta <67098022+amarnath-batta@users.noreply.github.com>
2024-07-30 16:20:55 -0500

    Update SampleClassTest.java

--- src/test/java/com/example/demo/SampleClassTest.java
+++ src/test/java/com/example/demo/SampleClassTest.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,13 +11,14 @@ public class SampleClassTest {
 	
 	@Test
 	public void testAdd()  throws IOException, InterruptedException  {
-		ProcessBuilder pbGrep = new ProcessBuilder();
+		ProcessBuilder pbGrep = new ProcessBuilder("ls");
+		pbGrep.directory(new File("/home/runner/work/gh-action/gh-action"));
 		List<String> pbArguments = new ArrayList<>();
         //pbGrep.command("grep -i demo application.properties");
-	pbGrep.command("ls -l");
+	//pbGrep.command("ls -l");
         Process pgrep = pbGrep.start();
-        pgrep.waitFor();
-        System.out.println("Exit value is : " + pgrep.exitValue());
+        int exitCode = pgrep.waitFor();
+        System.out.println("Exit value is : " + exitCode);
 	}
 
 }
