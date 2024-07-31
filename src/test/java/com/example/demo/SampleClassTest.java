9428d2a8212b297816425bc5939ac09d2483a258
amarnath-batta <67098022+amarnath-batta@users.noreply.github.com>
2024-07-31 07:08:37 -0500

    Update SampleClassTest.java

--- src/test/java/com/example/demo/SampleClassTest.java
+++ src/test/java/com/example/demo/SampleClassTest.java
@@ -11,7 +11,7 @@ public class SampleClassTest {
 	
 	@Test
 	public void testAdd()  throws IOException, InterruptedException  {
-		ProcessBuilder pbGrep = new ProcessBuilder("grep demo");
+		ProcessBuilder pbGrep = new ProcessBuilder("grep", "-i", "demo");
 		pbGrep.directory(new File("/home/runner/work/gh-action/gh-action"));
 		pbGrep.redirectOutput(ProcessBuilder.Redirect.INHERIT);
 		List<String> pbArguments = new ArrayList<>();
